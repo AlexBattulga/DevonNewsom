c8404868531e42d17280b04c00c87ed40e5f594c
WorldWideDev <dev206@gmail.com>
2018-04-16 11:29:55 -0700

    adding music-linq 2.0 template

--- 1_Week/WeekendSession/Program.cs
+++ 1_Week/WeekendSession/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WeekendSession
 {
@@ -6,7 +7,32 @@ namespace WeekendSession
     {
         static void Main(string[] args)
         {
-           
+            Hero dev = new Hero("Devon");
+
+            Enemy robot_1 = new Enemy("D34TH_30T");
+            Enemy robot_2 = new Enemy("Big Bag Robot");
+            Enemy soldier_1 = new Enemy("Guard");
+            Enemy soldier_2 = new Enemy("Guard");
+
+            List<Building> intersection = new List<Building>()
+            {
+                new Building(), new Building(), new Building(), new Building()
+            };
+
+            IDamageable[] thingsToAttack = new IDamageable[]
+            {
+                robot_1, robot_2, soldier_1, soldier_2,
+                new Building(), new Building()
+            };
+
+            dev.Attack(robot_1, 10);
+            dev.Attack(robot_2, 10);
+            dev.Attack(robot_2, 10);
+            dev.Attack(robot_2, 10);
+
+            dev.AOEAttack(intersection, 30);
+            dev.AOEAttack(thingsToAttack, 20);
+            
         }
     }
 }
